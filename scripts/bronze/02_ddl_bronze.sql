fade0349fe48fe455897823d44cb98ba42175c08
Angel C Pizarro <163421573+angelcpizarro@users.noreply.github.com>
2026-04-06 20:27:45 +0100

    Update DDL script for bronze tables
    Added BEGIN; and COMMIT; to DDL script.

--- scripts/bronze/02_ddl_bronze.sql
+++ scripts/bronze/02_ddl_bronze.sql
@@ -10,6 +10,8 @@ Script Purpose:
 ===============================================================================
 */
 
+BEGIN;
+
 -- Drop tables if they exist
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 DROP TABLE IF EXISTS bronze.crm_prd_info;
@@ -69,3 +71,5 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
     subcat       VARCHAR(50),
     maintenance  VARCHAR(50)
 );
+
+COMMIT;
